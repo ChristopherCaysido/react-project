2c782cbd099a94ac24b0d67cb8eeafb609b77db6
ChristopherCaysido <christopher.caysido@gmail.com>
2022-03-16 22:37:25 +0800

    Lab 1 - 5 Accomplished

---
+++ handsOnReact-Exercises/keeptrack/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
+
+function App() {
+  return (
+    <div className='container'>
+      <ProjectsPage/>
+    </div>
+  );
+}
+
+export default App;
