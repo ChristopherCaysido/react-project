cad40230cedc7d12722df4a494ff8f59abebdba3
Ezekiel Mariano <ezekielmariano27@yahoo.com>
2022-03-21 23:22:57 +0800

    Update ProjectList.js

--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -1,31 +1,37 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { Project } from './Project';
+import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
 
- function ProjectList({ projects }) {
-    return (
-        <div className="row">
-        {projects.map((project) => (
-        <div key={project.id} className="cols-sm">
-        <div className="card">
-        <img src={project.imageUrl} alt={project.name} />
-        <section className="section dark">
-        <h5 className="strong">
-        <strong>{project.name}</strong>
-        </h5>
-        <p>{project.description}</p>
-        <p>Budget : {project.budget.toLocaleString()}</p>
-        </section>
-        </div>
-        </div>
-        ))}
-        </div>
-        );
- }
+function ProjectList({ projects, onSave }) {
+        const [projectBeingEdited, setProjectBeingEdited] = useState({});
+        const handleEdit = (project) => {
+            setProjectBeingEdited(project);
+    };
+    const cancelEditing = () => {
+        setProjectBeingEdited({});
+    };
+    const items = projects.map(project => (
+      <div key={project.id} className="cols-sm">
+                 {project === projectBeingEdited ? (
+            <ProjectForm
+            onSave={onSave} 
+            onCancel={cancelEditing}
+            />
+          ) : (
+            <ProjectCard project={project} onEdit={handleEdit} />
+          )}
+      </div>
+    ));
+    return <div className="row">{items}</div>;
+}
+
+
 ProjectList.propTypes = {
- projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
- 
+   projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
+   onSave: PropTypes.func.isRequired
 };
 
 export default ProjectList;
